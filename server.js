b509b3ccf20e2fd603f484571f368b9447f3a2ca
Mako3 <big.mako@gmail.com>
2020-10-13 03:58:37 -0700

    deploying

--- server.js
+++ server.js
@@ -19,10 +19,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gBooks", {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 app.get("*", (req, res) => {

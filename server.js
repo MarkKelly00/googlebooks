27541da84912fe07a25dfef2b4101fee2836ff0a
Mako3 <big.mako@gmail.com>
2020-10-13 03:24:02 -0700

    new build deploy to heroku

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
+  useCreateIndex: true,
   useUnifiedTopology: true
 });
 

980265bd402171d0a39cb756a1117d25a65a1c40
Mako3 <big.mako@gmail.com>
2020-10-14 21:50:19 -0700

    run build

---
+++ client/build/precache-manifest.a5ee04a584f63b7aaabe6dfbab95b8fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b608298087fc58ccedb78cf28a2b22b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63cbebad823ca69c6af",
+    "url": "/static/css/main.83b52c26.chunk.css"
+  },
+  {
+    "revision": "c9e905bebefd0625c4ef",
+    "url": "/static/js/2.5401bb7c.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.5401bb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63cbebad823ca69c6af",
+    "url": "/static/js/main.5d961abb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
